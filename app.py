24dd20bf0ee06269ffe012c8050984b8ffe04c48
Bar Cohen <Barcohen056@gmail.com>
2023-08-16 22:48:35 +0300

    changed Mysql password

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask
 from flask_mysqldb import MySQL
+import Haltura.routes
 
 app = Flask(__name__, template_folder="./templates")
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'Kfir2208'
+app.config['MYSQL_PASSWORD'] = 'Haltura'
 app.config['MYSQL_DB'] = 'flask'
 mysql = MySQL(app)
 
-import Haltura.routes
