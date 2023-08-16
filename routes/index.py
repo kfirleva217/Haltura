c552249564460694f365d2faa987ba8c9286ecad
Bar Cohen <Barcohen056@gmail.com>
2023-08-16 22:29:58 +0300

    added login.html page, signup.html page

--- routes/index.py
+++ routes/index.py
@@ -5,3 +5,12 @@ from flask import render_template
 @app.route('/')
 def index():
     return render_template ('home.html')
+
+@app.route('/login', methods=['GET'])
+def login():
+    return render_template('login.html')
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    return render_template('signup.html')
+
