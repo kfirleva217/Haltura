9c774f308859a956495b7b59619154bf543dbf95
kfir <levari.kfir@gmail.com>
2023-08-24 14:19:17 +0300

    made an change after you singup it will return the login.html page besides the signup.html

--- app.py
+++ app.py
@@ -1,26 +1,12 @@
-from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
-# from flask_mysqldb import MySQL
-from flask_migrate import Migrate
+from flask_mysqldb import MySQL
+
 
 app = Flask(__name__, template_folder="./templates")
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///haltura.db'  # SQLite URI
-app.secret_key = 'haltura'
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-with app.app_context():
-    # Create the database tables
-    db.create_all()
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'Password'
+app.config['MYSQL_DB'] = 'Haltura'
+mysql = MySQL(app)
 
-
-
-
-
-
-from routes import index
-# app.config['MYSQL_HOST'] = 'localhost'
-# app.config['MYSQL_USER'] = 'root'
-# app.config['MYSQL_PASSWORD'] = 'admin'
-# app.config['MYSQL_DB'] = 'Haltura'
-# mysql = MySQL(app)
-
+from  routes import index
--- routes/users.py
+++ routes/users.py
@@ -1,11 +1,11 @@
-# from app import app, mysql
-# from flask import render_template
-#
-#
-# # @app.route('/users')
-# # def get_users():
-# #     cur = mysql.connection.cursor()
-# #     cur.execute("SELECT * FROM Haltura.clients")  # Make sure 'clients' table exists in the correct schema
-# #     clients = cur.fetchall()
-# #     return render_template('users.html', clients=clients)
-# #
+from app import app, mysql
+from flask import render_template
+
+
+@app.route('/users')
+def get_users():
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM Haltura.clients")  # Make sure 'clients' table exists in the correct schema
+    clients = cur.fetchall()
+    return render_template('users.html', clients=clients)
+
